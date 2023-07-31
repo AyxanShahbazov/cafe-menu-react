2929e8fb75272683013a95da472bd6412047e04e
Shahbazov <ayhanshahbazov@gmail.com>
2023-07-31 21:05:20 +0400

    Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Pizza from "./components/Pizza.jsx"
 
 function App() {
   return (
     <div className="App">
+
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Pizza.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Pizza() {
+    return (
+      <div>
+        <img src="pizzas/focaccia.jpg" alt="" />
+        <h1>Focaccia</h1>
+        <p>Bread with italian olive oil and rosemary</p>
+      </div>
+    );
+}
