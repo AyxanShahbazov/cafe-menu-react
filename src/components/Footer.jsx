745cddee9deddcd28e4721bcbceb2f6150b15a14
Shahbazov <ayhanshahbazov@gmail.com>
2023-08-01 18:53:54 +0400

    Small changes and description added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import Button from "./Button";
 const Footer = () => {
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 20;
+  const closeHour = 22;
   const isOpen = hour > 8 && hour < 22;
   console.log(isOpen);
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>
             We are <strong>open</strong> untill {closeHour}:00. Come visit us or
@@ -18,7 +18,8 @@ const Footer = () => {
           </p>
           <Button text="Order now" />
         </div>
-      )}
+      ) : <p>We are happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
+      }
     </footer>
   );
 };
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import "./data.js";
 
-const Pizza = (props) => {
-  console.log(props);
+const Pizza = ({pizzaObj}) => {
+  console.log(pizzaObj);
+
+  if (pizzaObj.soldOut) {
+    return null;
+  }
+
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </li>
   );
